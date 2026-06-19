5a5437a62a849aa833f7b56bb834f6ba0e4730a5
Greg Korte <gregmkorte@gmail.com>
2026-06-19 14:34:33 -0500

    Restructuring B2SA

--- src/sections/02-book-2/21-self-assessment/index.jsx
+++ src/sections/02-book-2/21-self-assessment/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "martins_aquarium_self_assessment",
+    id: "book_2_self_assessment",
     title: "Self-Assess JavaScript Fundamentals",
-    previousChapterId: "martins_aquarium_ma_filtering_fish",
-    nextChapterId: "martins_aquarium_explorer_duo_variables",
+    previousChapterId: "book_2_martins_aquarium_filtering_fish",
+    nextChapterId: "book_2_explorer_duo_variables",
     content,
 }
