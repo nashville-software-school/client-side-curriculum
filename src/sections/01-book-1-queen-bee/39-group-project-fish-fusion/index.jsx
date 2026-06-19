973e1f64514ec8273c10c1af91ae17e32516f3a9
Greg Korte <gregmkorte@gmail.com>
2026-06-19 13:48:12 -0500

    Restructuring B1GP

--- src/sections/01-book-1-queen-bee/39-group-project-fish-fusion/index.jsx
+++ src/sections/01-book-1-queen-bee/39-group-project-fish-fusion/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "queen_bee_group_project_fish_fusion",
+    id: "book_1_group_project_fish_fusion",
     title: "Fish Fusion",
-    previousChapterId: "queen_bee_explorer_bjorn_array_find",
+    previousChapterId: "book_1_explorer_bjorn_array_find",
     nextChapterId: null,
     content,
 }
