9943489ec29fe1eb3b10c0baf078de107b074245
Greg Korte <gregmkorte@gmail.com>
2026-06-19 16:15:57 -0500

    Restructuring B4SA

--- src/sections/04-book-4/29-self-assessment/index.jsx
+++ src/sections/04-book-4/29-self-assessment/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "kneel_diamonds_self_assessment",
+    id: "book_4_self_assessment",
     title: "Async, State, and Array Method Self-Assessment",
-    previousChapterId: "kneel_diamonds_cb_build_cost",
-    nextChapterId: "kneel_diamonds_explorer_ij_more_inputs",
+    previousChapterId: "book_4_cars_r_us_build_cost",
+    nextChapterId: "book_4_explorer_indiana_jeans_more_inputs",
     content,
 }
