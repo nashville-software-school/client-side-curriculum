1661963a017bfba2efcac928aa6c6f51fc9a8aae
Greg Korte <gregmkorte@gmail.com>
2026-06-19 14:35:31 -0500

    Restructuring B2EP

--- src/sections/02-book-2/22-explorer-duo-variables/index.jsx
+++ src/sections/02-book-2/22-explorer-duo-variables/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "martins_aquarium_explorer_duo_variables",
+    id: "book_2_explorer_duo_variables",
     title: "Are you primitive?",
-    previousChapterId: "martins_aquarium_self_assessment",
-    nextChapterId: "martins_aquarium_explorer_duo_copies",
+    previousChapterId: "book_2_self_assessment",
+    nextChapterId: "book_2_explorer_duo_copies",
     content,
 }
