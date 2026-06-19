535c69d22a2859fb4301c6cad52b4379fd1166f4
Greg Korte <gregmkorte@gmail.com>
2026-06-19 15:21:56 -0500

    Restructuring B3GP

--- src/sections/03-book-3/25-group-project-cider-falls/index.jsx
+++ src/sections/03-book-3/25-group-project-cider-falls/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "deshawns_dog_walking_group_project_cider_falls",
+    id: "book_3_group_project_cider_falls",
     title: "Cider Falls Park",
-    previousChapterId: "deshawns_dog_walking_self_assessment",
+    previousChapterId: "book_3_self_assessment",
     nextChapterId: null,
     content,
 }
