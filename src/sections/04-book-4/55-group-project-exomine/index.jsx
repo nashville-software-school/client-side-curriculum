070afdf879fb07e624184b21d8aa1b867d450dbe
Greg Korte <gregmkorte@gmail.com>
2026-06-20 12:38:52 -0500

    Restructuring B4GP

--- src/sections/04-book-4/55-group-project-exomine/index.jsx
+++ src/sections/04-book-4/55-group-project-exomine/index.jsx
@@ -1,9 +1,9 @@
 import content from "./index.md?raw";
 
 export default {
-    id: "kneel_diamonds_group_project_exomine",
+    id: "book_4_group_project_exomine",
     title: "Exomine",
-    previousChapterId: "kneel_diamonds_holiday_road",
+    previousChapterId: "book_4_advanced_holiday_road",
     nextChapterId: null,
     content,
 }
